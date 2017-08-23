e4b8afcf383e3787cd440b0b853fcbd233282c91
matamegger <matthias.tamegger@bitmovin.com>
2017-08-23 13:15:49 +0200

    Add copyright file header

--- BasicPlayback/src/main/java/com/bitmovin/player/samples/playback/basic/MainActivity.java
+++ BasicPlayback/src/main/java/com/bitmovin/player/samples/playback/basic/MainActivity.java
@@ -1,3 +1,11 @@
+/*
+ * Bitmovin Player Android SDK
+ * Copyright (C) 2017, Bitmovin GmbH, All Rights Reserved
+ *
+ * This source code and its use and distribution, is subject to the terms
+ * and conditions of the applicable license agreement.
+ */
+
 package com.bitmovin.player.samples.playback.basic;
 
 import android.os.Bundle;
