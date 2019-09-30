c533f8d1ac693ef127c8f73ae39ce831b77d49a7
David Steinacher <david.steinacher@bitmovin.com>
2019-09-30 15:38:10 +0200

    Update sample apps to use Bitmovin Player SDK 2.31.0

---
+++ CustomAdaptation/src/main/java/com/bitmovin/player/samples/custom/adaptation/MainActivity.java
@@ -0,0 +1,100 @@
+package com.bitmovin.player.samples.custom.adaptation;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+import com.bitmovin.player.BitmovinPlayer;
+import com.bitmovin.player.BitmovinPlayerView;
+import com.bitmovin.player.config.AdaptationConfiguration;
+import com.bitmovin.player.config.PlayerConfiguration;
+import com.bitmovin.player.config.adaptation.VideoAdaptation;
+import com.bitmovin.player.config.adaptation.data.VideoAdaptationData;
+import com.bitmovin.player.config.media.SourceConfiguration;
+import com.bitmovin.player.config.quality.VideoQuality;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BitmovinPlayer bitmovinPlayer;
+    private BitmovinPlayerView bitmovinPlayerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PlayerConfiguration playerConfiguration = this.createPlayerConfiguration();
+        this.bitmovinPlayerView = new BitmovinPlayerView(this, playerConfiguration);
+        this.bitmovinPlayer = this.bitmovinPlayerView.getPlayer();
+
+        LinearLayout rootView = this.findViewById(R.id.root);
+        this.bitmovinPlayerView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        rootView.addView(this.bitmovinPlayerView, 0);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        this.bitmovinPlayerView.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        this.bitmovinPlayerView.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        this.bitmovinPlayerView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        this.bitmovinPlayerView.onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        this.bitmovinPlayerView.onDestroy();
+        super.onDestroy();
+    }
+
+    private PlayerConfiguration createPlayerConfiguration() {
+        // Setup adaptation configuration
+        AdaptationConfiguration adaptationConfiguration = new AdaptationConfiguration();
+        adaptationConfiguration.setAllowRebuffering(true);
+        adaptationConfiguration.setMaxSelectableVideoBitrate(800_000);
+        adaptationConfiguration.setStartupBitrate(1_200_000);
+        adaptationConfiguration.setVideoAdaptation(videoAdaptationListener);
+
+        // Setup source configuration
+        SourceConfiguration sourceConfiguration = new SourceConfiguration();
+        sourceConfiguration.addSourceItem("https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd");
+
+        // Assign adaptation and source configuration to player configuration
+        PlayerConfiguration playerConfiguration = new PlayerConfiguration();
+        playerConfiguration.setAdaptationConfiguration(adaptationConfiguration);
+        playerConfiguration.setSourceConfiguration(sourceConfiguration);
+        return playerConfiguration;
+    }
+
+    private VideoAdaptation videoAdaptationListener = new VideoAdaptation() {
+        /*
+         *  Customize this method to return a different video quality id than what is suggested
+         */
+        @Override
+        public String onVideoAdaptation(VideoAdaptationData videoAdaptationData) {
+            // Get the suggested video quality id
+            String suggestedVideoQualityId = videoAdaptationData.getSuggested();
+
+            // Add your own logic to choose a different video quality
+            VideoQuality[] videoQualities = bitmovinPlayer.getAvailableVideoQualities();
+
+            return suggestedVideoQualityId;
+        }
+    };
+}
---
+++ CustomAdaptationKotlin/src/main/java/com/bitmovin/player/samples/custom/adaptation/MainActivity.kt
@@ -0,0 +1,88 @@
+package com.bitmovin.player.samples.custom.adaptation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.LinearLayout
+import com.bitmovin.player.BitmovinPlayer
+import com.bitmovin.player.BitmovinPlayerView
+import com.bitmovin.player.config.AdaptationConfiguration
+import com.bitmovin.player.config.PlayerConfiguration
+import com.bitmovin.player.config.adaptation.VideoAdaptation
+import com.bitmovin.player.config.media.SourceConfiguration
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private var bitmovinPlayer: BitmovinPlayer? = null
+    private var bitmovinPlayerView: BitmovinPlayerView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val playerConfiguration = this.createPlayerConfiguration()
+        this.bitmovinPlayerView = BitmovinPlayerView(this, playerConfiguration)
+        this.bitmovinPlayer = this.bitmovinPlayerView?.player
+
+        this.bitmovinPlayerView?.layoutParams = LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT)
+        root.addView(this.bitmovinPlayerView, 0)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        this.bitmovinPlayerView?.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        this.bitmovinPlayerView?.onResume()
+    }
+
+    override fun onPause() {
+        this.bitmovinPlayerView?.onPause()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        this.bitmovinPlayerView?.onStop()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        this.bitmovinPlayerView?.onDestroy()
+        super.onDestroy()
+    }
+
+    private fun createPlayerConfiguration(): PlayerConfiguration {
+        // Setup adaptation configuration
+        val adaptationConfiguration = AdaptationConfiguration()
+        adaptationConfiguration.isAllowRebuffering = true
+        adaptationConfiguration.maxSelectableVideoBitrate = 800_000
+        adaptationConfiguration.startupBitrate = 1_200_000
+        adaptationConfiguration.videoAdaptation = videoAdaptationListener
+
+        // Setup source configuration
+        val sourceConfiguration = SourceConfiguration()
+        sourceConfiguration.addSourceItem("https://bitdash-a.akamaihd.net/content/sintel/sintel.mpd")
+
+        // Assign adaptation and source configuration to player configuration
+        val playerConfiguration = PlayerConfiguration()
+        playerConfiguration.adaptationConfiguration = adaptationConfiguration
+        playerConfiguration.sourceConfiguration = sourceConfiguration
+        return playerConfiguration
+    }
+
+    /*
+     *  Customize this callback to return a different video quality id than what is suggested
+     */
+    private val videoAdaptationListener = VideoAdaptation { videoAdaptationData ->
+        // Get the suggested video quality id
+        val suggestedVideoQualityId = videoAdaptationData.suggested
+
+        // Add your own logic to choose a different video quality
+        val videoQualities = bitmovinPlayer?.availableVideoQualities
+
+        // Return video quality id
+        suggestedVideoQualityId
+    }
+}
