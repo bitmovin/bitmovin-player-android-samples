f2087ece0b2b40304a620ec4057af528730b6ec6
Lukas Knoch-Girstmair <lukas.knoch-girstmair@bitmovin.com>
2026-03-30 08:37:33 +0200

    Updat samples

--- MultiView/build.gradle.kts
+++ MultiView/build.gradle.kts
@@ -29,8 +29,10 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
     }
     buildFeatures {
         compose = true
